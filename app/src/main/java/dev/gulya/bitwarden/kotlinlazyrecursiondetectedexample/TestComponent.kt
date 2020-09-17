7dbd6c12eb04eb1fdc7c8a600a458858b6f80ef9
Ilya Gulya <ilyagulya@gmail.com>
2020-09-17 14:29:50 +0300

    Minimal reproducible example

--- app/src/main/java/dev/gulya/bitwarden/kotlinlazyrecursiondetectedexample/TestComponent.kt
+++ app/src/main/java/dev/gulya/bitwarden/kotlinlazyrecursiondetectedexample/TestComponent.kt
@@ -1,11 +1,7 @@
 package dev.gulya.bitwarden.kotlinlazyrecursiondetectedexample
 
-import javax.inject.Scope
-
 @TestComponent.TestScope
 interface TestComponent {
 
-    @Retention(AnnotationRetention.RUNTIME)
-    @Scope
     annotation class TestScope
 }
